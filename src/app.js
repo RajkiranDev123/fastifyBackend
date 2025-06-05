4c38e59c91da650e3c9a4bb32b6a1c8cb8204bb9
raj <rajtech645@gmail.com>
2025-06-05 11:27:03 +0530

    fastify

---
+++ src/app.js
@@ -0,0 +1,14 @@
+const fastify = require("fastify")({ logger: true });
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+
+// Import my routes
+
+
+// Connect to my database
+
+
+
+
+// start my server
