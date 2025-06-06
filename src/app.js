cd6b86646240ce44a85ea956fd4d8f79fe5c4bdb
raj <rajtech645@gmail.com>
2025-06-06 08:57:14 +0530

    fastify

--- src/app.js
+++ src/app.js
@@ -2,18 +2,20 @@ const fastify = require("fastify")({ logger: true });
 const mongoose = require("mongoose");
 require("dotenv").config();
 
-
 // Import my routes
 const userRoutes = require("./routes/user.routes");
 
-
-
-
 // Connect to my database
+mongoose
+    .connect(process.env.DATABASE_URL)
+    .then(() => console.log("Connected to the database"))
+    .catch((e) => console.log("Error connecting to database", e));
+
 
 
 // start my server
 //mount routes
+//http://localhost:5000/api/v1/users/
 fastify.register(userRoutes, { prefix: "/api/v1/users" });
 
 const start = async () => {
